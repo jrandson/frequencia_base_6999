c6dceb0556005e9a5f070703deb8455b549f90d4
Randson Cunha <jrandson@gmail.com>
2025-07-29 22:14:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if stripe.api_key:
         limit_exec = st.toggle("Atualizar apenas 3 inscrições")
         if limit_exec:    
             total_executions = 3
-            st.caption(f"Esta ação irá alterar {limit_exec} inscrições ativas para fins de teste.")
+            st.caption(f"Esta ação irá alterar {total_executions} inscrições ativas para fins de teste.")
         else:
             st.caption("Esta ação irá alterar todas as incrições ativas para o novo valor.")
     except:
